de883013c0007666235502ba4e8b9f293c882fd7
Tayebwa-ian <tayebwaian0@gmail.com>
2024-06-13 23:19:35 +0000

    sql statements

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- creates a table users on any Database
+CREATE TABLE IF NOT EXISTS users (
+       id INT AUTO_INCREMENT NOT NULL,
+       email VARCHAR(255) UNIQUE NOT NULL,
+       name VARCHAR(255),
+       country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL,
+       PRIMARY KEY (ID)
+);
