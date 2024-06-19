6fd291466b926e61d9350ff9afec8bba0e2a1bbd
Tayebwa-ian <tayebwaian0@gmail.com>
2024-06-19 19:01:09 +0000

    setting up a Redis instance

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Create a Redis instance"""
+import redis
+from typing import Union
+import uuid
+
+
+class Cache:
+    """Working with Redis Server"""
+    def __init__(self):
+        """Intialise Cache class"""
+        self._redis = redis.Redis()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        store input data into the redis sever
+        Arg:
+          data: the input data to store
+        return: key(str) for the stored value
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
