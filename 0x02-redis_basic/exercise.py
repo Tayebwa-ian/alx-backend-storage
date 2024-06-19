39e5a3f845695673a2129eed88d39616fe28433f
Tayebwa-ian <tayebwaian0@gmail.com>
2024-06-19 19:20:02 +0000

    setting up a Redis instance

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -10,6 +10,7 @@ class Cache:
     def __init__(self):
         """Intialise Cache class"""
         self._redis = redis.Redis()
+        self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
